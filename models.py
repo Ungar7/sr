76e77271c3db68a0f28900b4d2f80bc66cfd5a82
Weinan <zwn1989@gmail.com>
2016-07-16 00:05:54 +0800

    new model

--- models.py
+++ models.py
@@ -14,20 +14,21 @@ def convolutional_dnn(channels, input_patch, output_patch):
     model.add(Convolution2D(32, 1, 1, activation='relu'))
 
     model.add(ZeroPadding2D((2, 2)))
-    model.add(Convolution2D(1, 5, 5, activation='sigmoid'))
+    model.add(Convolution2D(1, 5, 5, activation='relu'))
 
     #model.add(ZeroPadding2D((1, 1)))
     #model.add(Activation('relu'))
     #model.add(Convolution2D(channels, 3, 3))
 
     model.add(Flatten())
-    model.add(Dense(channels * output_patch * output_patch))
+    model.add(Dense(channels * output_patch * output_patch, activation='tanh'))
+    model.add(Dense(channels * output_patch * output_patch, activation='sigmoid'))
 
     # let's try sgd as the paper said
-    sgd = SGD(lr=0.0001, decay=0, momentum=0.9)
-    model.compile(loss='mean_squared_error', optimizer=sgd)
+    #sgd = SGD(lr=0.0001, decay=0, momentum=0.9)
+    #model.compile(loss='mean_squared_error', optimizer=sgd)
 
-    #model.compile(loss='mse', optimizer=Adam())
+    model.compile(loss='mse', optimizer=Adam())
 
     return model
 
